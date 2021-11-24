37e739d283ca3e82b6216a139b77762748b5eadd
SHowardGal <u0250173@utah.edu>
2021-11-23 17:28:30 -0700

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+if( 1 ==="1" ) {
+    console.log("Strict");
+} else if( 1 == "1") {
+    console.log("loose");
+} else {
+    console.log("not equal at all");
+}
+if( 1 <= 2 && 2 === 4) {
+    console.log("yes");
+} else {
+    console.log("no");
+}
